1841b16a7451ffc8c735d04180b28169963eb3a7
Eddie <adward_r@icloud.com>
2016-10-21 22:12:49 -0400

    Lab 02: Add Graph Checkpoints Major Functions

--- graph.hpp
+++ graph.hpp
@@ -5,10 +5,13 @@
 #include <map>
 #include <set>
 #include <stdint.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 class Graph{
 	private:
 		std::map<uint64_t,std::set<uint64_t> > nodes;
+		int access_ckpoint(int fd, uint64_t cache[], int cache_length);
 	public:
 		Graph();
 		// return 1 on success, 0 if the node exists
@@ -20,5 +23,6 @@ class Graph{
 		int get_edge(uint64_t a, uint64_t b);
 		int get_neighbors(uint64_t node_id, std::vector<uint64_t> &res);
 		int shortest_path(uint64_t a, uint64_t b);
+		int checkpoint();
 };
 #endif /* GRAPH_HPP */
