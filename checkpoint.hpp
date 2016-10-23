329ae2200a357f1e76c1546f51675f300865ce24
liyuliang001 <liyuliang001@instance-1.c.distributed-system-keyang.internal>
2016-10-23 18:45:28 +0000

    add comments to checkpoint

--- checkpoint.hpp
+++ checkpoint.hpp
@@ -5,9 +5,15 @@
 
 class CheckPoint{
 	private:
-		int access_ckpoint(uint64_t cache[], int cache_length, int offset, int opt);
-		int restore();
+		//int access_ckpoint(uint64_t cache[], int cache_length, int offset, int opt);
+		//int restore();
+		int fd; // file descriptor
+		char buf[BLK_SIZE]; // the buffer to hold write; write to disk every BLK_SIZE
+		int buf_tail; // point to the tail of the buffer
 	public:
-		int checkpoint();
+		//int checkpoint();
+		int init(); // init the fd, buf, buf_tail
+		int write(string &s); // called by other objects (graph), to write to disk
+		int read(char* ret, int len); // len is the number of bytes, ret is the return buffer
 };
 #endif /* CHECKPOINT_HPP */
